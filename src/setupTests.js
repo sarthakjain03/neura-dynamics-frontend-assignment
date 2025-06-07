672582ee16cc2f0ec0b3a397b3915a41c88941c7
Sarthak Jain <jainsarthak2003@gmail.com>
2025-06-07 16:29:30 +0530

    for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom';
+
+// Polyfill TextEncoder and TextDecoder for Jest environment (Node.js)
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
